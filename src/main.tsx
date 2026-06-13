41818a3be749e9a502b595c1b168e8a4db3c99d8
Abdul Shakoor <ibupoto@gmail.com>
2026-06-13 13:46:11 +0500

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Old Default code..........................
+
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
